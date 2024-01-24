a9ecd9b32963d603ac81e9021f4d6208d86ba34a
RN Developer <allen.gzqyl@gmail.com>
2024-01-24 19:05:40 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     targets: [
         .target(
             name: "Refreshable",
-            dependencies: ["Introspect"],
+            dependencies: ["SwiftUIIntrospect"],
 			path: "Refreshable"
 		)
     ]
